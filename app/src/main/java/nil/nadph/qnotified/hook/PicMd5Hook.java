b1d5ca7f2f2dc71405ab085e3e8f29cd98ddfcf8
Null Idle <xenonhydride@gmail.com>
2021-02-14 22:37:35 +0800

    pick some modify in PR565
    添加图片md5开关

--- app/src/main/java/nil/nadph/qnotified/hook/PicMd5Hook.java
+++ app/src/main/java/nil/nadph/qnotified/hook/PicMd5Hook.java
@@ -1,6 +1,6 @@
 /* QNotified - An Xposed module for QQ/TIM
- * Copyright (C) 2019-2020 xenonhydride@gmail.com
- * https://github.com/cinit/QNotified
+ * Copyright (C) 2019-2021 xenonhydride@gmail.com
+ * https://github.com/ferredoxin/QNotified
  *
  * This software is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -33,26 +33,21 @@ import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import nil.nadph.qnotified.MainHook;
 import nil.nadph.qnotified.R;
-import nil.nadph.qnotified.SyncUtils;
 import nil.nadph.qnotified.config.ConfigManager;
-import nil.nadph.qnotified.step.Step;
+import nil.nadph.qnotified.util.Initiator;
 import nil.nadph.qnotified.ui.CustomDialog;
 import nil.nadph.qnotified.util.CustomMenu;
-import nil.nadph.qnotified.util.Initiator;
 import nil.nadph.qnotified.util.LicenseStatus;
 
 import static nil.nadph.qnotified.util.Initiator.load;
-import static nil.nadph.qnotified.util.Utils.TOAST_TYPE_ERROR;
-import static nil.nadph.qnotified.util.Utils.iget_object_or_null;
-import static nil.nadph.qnotified.util.Utils.log;
-import static nil.nadph.qnotified.util.Utils.showToast;
+import static nil.nadph.qnotified.util.ReflexUtil.iget_object_or_null;
+import static nil.nadph.qnotified.util.Utils.*;
 
-public class PicMd5Hook extends BaseDelayableHook {
-    public static final String qn_show_pic_md5 = "qn_show_pic_md5";
+public class PicMd5Hook extends CommonDelayableHook {
     private static final PicMd5Hook self = new PicMd5Hook();
-    private boolean inited = false;
 
     PicMd5Hook() {
+        super("qn_show_pic_md5");
     }
 
     public static PicMd5Hook get() {
@@ -60,8 +55,7 @@ public class PicMd5Hook extends BaseDelayableHook {
     }
 
     @Override
-    public boolean init() {
-        if (inited) return true;
+    public boolean initOnce() {
         try {
             Class cl_PicItemBuilder = Initiator._PicItemBuilder();
             Class cl_BasePicItemBuilder = cl_PicItemBuilder.getSuperclass();
@@ -83,7 +77,6 @@ public class PicMd5Hook extends BaseDelayableHook {
                     break;
                 }
             }
-            inited = true;
             return true;
         } catch (Throwable e) {
             log(e);
@@ -99,11 +92,7 @@ public class PicMd5Hook extends BaseDelayableHook {
         @Override
         protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
             if (LicenseStatus.sDisableCommonHooks) return;
-            try {
-                ConfigManager cfg = ConfigManager.getDefaultConfig();
-                if (!PicMd5Hook.get().isEnabled()) return;
-            } catch (Exception ignored) {
-            }
+            if (!PicMd5Hook.get().isEnabled()) return;
             Object arr = param.getResult();
             Class<?> clQQCustomMenuItem = arr.getClass().getComponentType();
             Object item_copy = CustomMenu.createItem(clQQCustomMenuItem, R.id.item_showPicMd5, "MD5");
@@ -133,12 +122,9 @@ public class PicMd5Hook extends BaseDelayableHook {
                         showToast(ctx, TOAST_TYPE_ERROR, "获取图片MD5失败", Toast.LENGTH_SHORT);
                         return;
                     }
-                    CustomDialog.createFailsafe(ctx).setTitle("MD5").setCancelable(true).setMessage(md5).setPositiveButton("复制", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            ClipboardManager clipboardManager = (ClipboardManager) ctx.getSystemService(Context.CLIPBOARD_SERVICE);
-                            clipboardManager.setPrimaryClip(ClipData.newPlainText(null, md5));
-                        }
+                    CustomDialog.createFailsafe(ctx).setTitle("MD5").setCancelable(true).setMessage(md5).setPositiveButton("复制", (dialog, which) -> {
+                        ClipboardManager clipboardManager = (ClipboardManager) ctx.getSystemService(Context.CLIPBOARD_SERVICE);
+                        clipboardManager.setPrimaryClip(ClipData.newPlainText(null, md5));
                     }).setNegativeButton("关闭", null).show();
                 } catch (Throwable e) {
                     log(e);
@@ -146,52 +132,5 @@ public class PicMd5Hook extends BaseDelayableHook {
                 }
             }
         }
-    }
-
-    @Override
-    public int getEffectiveProc() {
-        return SyncUtils.PROC_MAIN;
-    }
-
-    @Override
-    public Step[] getPreconditions() {
-        return new Step[]{};
-    }
-
-    @Override
-    public boolean isInited() {
-        return inited;
-    }
-
-    @Override
-    public void setEnabled(boolean enabled) {
-//        try {
-//            ConfigManager mgr = ConfigManager.getDefaultConfig();
-//            mgr.getAllConfig().put(qn_show_pic_md5, enabled);
-//            mgr.save();
-//        } catch (final Exception e) {
-//            Utils.log(e);
-//            if (Looper.myLooper() == Looper.getMainLooper()) {
-//                Utils.showToast(getApplication(), TOAST_TYPE_ERROR, e + "", Toast.LENGTH_SHORT);
-//            } else {
-//                SyncUtils.post(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        Utils.showToast(getApplication(), TOAST_TYPE_ERROR, e + "", Toast.LENGTH_SHORT);
-//                    }
-//                });
-//            }
-//        }
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-//        try {
-//            return ConfigManager.getDefaultConfig().getBooleanOrFalse(qn_show_pic_md5);
-//        } catch (Exception e) {
-//            log(e);
-//            return false;
-//        }
     }
 }
