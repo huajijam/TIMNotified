f05b431dda632e3ddec954e9d3ae8c00b7c9f45b
‭huajijam <strhuaji@gmail.com>
2021-07-23 04:59:06 +0800

    pick some modify in PR412
    添加自动续火

--- app/src/main/java/me/kyuubiran/utils/ConfigManager.kt
+++ app/src/main/java/me/kyuubiran/utils/ConfigManager.kt
@@ -1,7 +1,12 @@
 package me.kyuubiran.utils
 
+import nil.nadph.qnotified.ExfriendManager
 import nil.nadph.qnotified.config.ConfigManager
 
 fun getDefaultCfg(): ConfigManager {
     return ConfigManager.getDefaultConfig()
+}
+
+fun getExFriendCfg(): ConfigManager {
+    return ExfriendManager.getCurrent().config
 }
